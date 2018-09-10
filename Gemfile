bb37d67cc65d4b3122b1c328eb3d9422c24d45fb
mguevara717 <marissa.guevara717@gmail.com>
2018-09-10 17:55:31 -0400

    added methods for the developer model

--- Gemfile
+++ Gemfile
@@ -3,7 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.3.3'
 
-
+gem 'bootstrap'
+gem 'jquery-rails'
+#bootstrap
 gem 'faker'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,9 +1,35 @@
 class Developer < ApplicationRecord
   has_many :projects
+  has_many :languages, through: :projects
   has_many :customers, through: :projects
   has_many :reviews, through: :projects
 
   def my_languages
-    
+    self.languages.uniq.pluck(:name)
   end
-end
+
+  def my_reviews
+    #code
+  end
+
+  def average_rating
+    #code
+  end
+
+  def get_total_projects
+    #code
+  end
+
+  def amt_earned
+    #code
+  end
+
+end##END OF CLASS
+
+
+
+#my_languages // list all the languages .uniq (of all languages)
+#review //list of all reviews (self)
+#average_rating //average rating of all reviews
+#get_total_projects //return the size of the projects accrued
+#amt_earned // sum of the price for all projects that were accepted
