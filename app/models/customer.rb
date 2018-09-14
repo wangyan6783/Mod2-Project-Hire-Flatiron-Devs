c5c6e11d525635a01d70273617fb84576408f432
mguevara717 <marissa.guevara717@gmail.com>
2018-09-14 16:10:29 -0400

    fixed bug

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,6 @@ class Customer < ApplicationRecord
   has_secure_password
   has_many :projects
   has_many :developers, through: :projects
-
   validates :email, uniqueness: true
   validates :password, :email, presence: true
 
